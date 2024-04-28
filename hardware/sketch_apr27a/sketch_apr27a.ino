c24516b47794c7d4a2ad3bb26f39415f943f34d0
HarnoorC <harnoorchima1@gmail.com>
2024-04-27 22:11:02 -0700

    Refactor brake sensor code

--- hardware/sketch_apr27a/sketch_apr27a.ino
+++ hardware/sketch_apr27a/sketch_apr27a.ino
@@ -1,18 +1,22 @@
 const int speakerPin = 6;
-const int redPin = 5;
-const int bluePin = 4;
-const int greenPin = 3;
+
+const int redLeftPin = 5;
+const int blueLeftPin = 4;
+const int greenLeftPin = 3;
+
+const int redRightPin = 11;
+const int blueRightPin = 10;
+const int greenRightPin = 9;
 
 const int trigPin = 13;
 const int echoPin = 12;
 
 void setup() {
-  // put your setup code here, to run once:
-  serial.begin(9600);
+  Serial.begin(9600);
   pinMode(speakerPin, OUTPUT);
-  pinMode(redPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
+  pinMode(redLeftPin, OUTPUT);
+  pinMode(blueLeftPin, OUTPUT);
+  pinMode(greenLeftPin, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
@@ -27,63 +31,67 @@ void pingSensor() {
   digitalWrite(trigPin, LOW);
 }
 
-void checkSensor() {
-  Serial.print("CHECKING BRAKE SENSOR");
-  pingSensor();
+float updateDistance() {
   // Read the echoPin, returns the sound wave travel time in microseconds
   long duration = pulseIn(echoPin, HIGH);
   // Convert the time into a distance (cm)
-  float distance_cm = duration * 0.034 / 2;
+  float distance_m = (duration * 0.034 / 2) / 100;
   // Print distance to serial monitor
   Serial.print("Distance: ");
-  Serial.print(distance_cm);
-  Serial.println(" cm");
+  Serial.print(distance_m);
+  Serial.println(" m");
 
-  while (distance < 100) {
+  return distance_m;
+}
+
+void checkSensor() {
+  Serial.print("CHECKING BRAKE SENSOR\n");
+  pingSensor();
+  float distance = updateDistance();
+
+  while (distance < 4.5) {
+    pingSensor();
+    distance = updateDistance();
     digitalWrite(speakerPin, HIGH);
-    delay(500)
+    delay(250);
+    digitalWrite(speakerPin, LOW);
+    delay(250);
   }
   
   digitalWrite(speakerPin, LOW);
 }
 
 void checkLane() {
-  Serial.print("CHECKING LANE ASSIST")
+  Serial.print("CHECKING LANE ASSIST\n");
   if (Serial.available() > 0) {
     char data = Serial.read();
-    if (data == "BRAKE_SIG") {
-      Serial.print("TODO: BRAKING BEHAVIOR")
+    if (data == "LANE_LEFT_SIG") {
+      
     }
   }
 }
 
 void checkFace() {
-  Serial.print("CHECKING FACE")
+  Serial.print("CHECKING FACE\n");
   if (Serial.available() > 0) {
     char data = Serial.read();
     if (data == "FACE_SIG") {
-      Serial.print("TODO: FACE DETECTION")
+      Serial.print("TODO: FACE DETECTION");
     }
   }
 }
 
 void checkBSM() {
-  Serial.print("CHECKING BLIND SPOT MONITOR")
+  Serial.print("CHECKING BLIND SPOT MONITOR\n");
   if (Serial.available() > 0) {
     char data = Serial.read();
     if (data == "BSM_SIG") {
-      Serial.print("TODO: BSM BEHAVIOR")
+      Serial.print("TODO: BSM BEHAVIOR");
     }
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(6, HIGH);
-  delay(1000);
-  digitalWrite(6, LOW);
-  delay(1000); 
-
   checkSensor();
   checkLane();
   checkFace();
